f2fdea57473096d4989ff68294e954321ab91d5a
ThongWan1706 <S10275182@connect.np.edu.sg>
2026-07-09 20:46:34 +0800

    Update of goal area
    Update of goal area code as the points were doubled due to the double collider on the ball, fixed it to ensure the points are always +1

--- Assets/Script/Goalarea.cs
+++ Assets/Script/Goalarea.cs
@@ -3,16 +3,29 @@ using UnityEngine;
 public class GoalArea : MonoBehaviour
 {
     [SerializeField] private int scoreValue = 1; 
+    private bool isBallInside = false; 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Ball"))
+        // Only score if it's the ball and we haven't already registered it
+        if (other.CompareTag("Ball") && !isBallInside)
         {
+            isBallInside = true; 
+
             PlayerScript player = FindFirstObjectByType<PlayerScript>();
             if (player != null)
             {
                 player.AddScore(scoreValue);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Reset the flag when the ball completely leaves the zone
+        if (other.CompareTag("Ball"))
+        {
+            isBallInside = false; 
         }
     }
 }
